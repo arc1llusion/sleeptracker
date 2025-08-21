aa2b7437952e803c0e9a252741e971ceecc325a3
Arc1llusion <arc1llusion@gmail.com>
2025-08-21 11:38:16 -0400

    dumb errors

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,3 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './login.component';
-import { CallbackComponent } from './callback.component';
 
-export const routes: Routes = [
-{
-	path: 'login',
-	component: LoginComponent
-},
-{
-	path: 'callback',
-	component: CallbackComponent
-}];
+export const routes: Routes = [];
